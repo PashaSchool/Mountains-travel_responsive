6ff411b47a668b4fa2dcf51557112e2b3bc91546
Pasha School <pasha.school888@gmail.com>
2017-02-04 23:00:42 +0200

    empty proj evn.

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require("gulp"),
+	notify = require("gulp-notify"),
+	compass = require("gulp-compass"),
+	browsersync = require("browser-sync"),
+	gulfif = require("gulp-if"),
+	concat = require("gulp-concat");
+
+gulp.task();
