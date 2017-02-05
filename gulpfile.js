281dde29d4418440a6c2885470ca0650bcb514f5
Pasha School <pasha.school888@gmail.com>
2017-02-06 01:09:51 +0200

    nav + intro section is ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,46 @@
 var gulp = require("gulp"),
-	notify = require("gulp-notify"),
-	compass = require("gulp-compass"),
-	browsersync = require("browser-sync"),
-	gulfif = require("gulp-if"),
-	concat = require("gulp-concat");
+    notify = require("gulp-notify"),
+    compass = require("gulp-compass"),
+    browsersync = require("browser-sync"),
+    gulfif = require("gulp-if"),
+    concat = require("gulp-concat");
 
 var env = "develpment",
-	sassFolder,
-	outputDir,
-	sassStyle;
+    sassFolder,
+    outputDir,
+    sassStyle;
 
-if (env==='develpment') {
-  outputDir = 'builds/develpment/';
-  sassStyle = 'expanded';
+if (env === 'develpment') {
+    outputDir = 'builds/develpment/';
+    sassStyle = 'expanded';
 } else {
-  outputDir = 'builds/production/';
-  sassStyle = 'compressed';
+    outputDir = 'builds/production/';
+    sassStyle = 'compressed';
 }
 
 sassFolder = ['components/style/style.scss'];
 
 gulp.task('compass', function() {
-	return gulp.src(sassFolder)
-		.pipe(compass({
-			sass: 'components/style/',
-			css: outputDir + "css",
-			image: outputDir + "images",
-			require: ['susy', 'breakpoint']
-		})).on('error', notify.onError())
-		.pipe(gulp.dest(outputDir + "css"))
-		.pipe(browsersync.stream());
+    return gulp.src(sassFolder)
+        .pipe(compass({
+            sass: 'components/style/',
+            css: outputDir + "css",
+            image: outputDir + "images",
+            require: ['susy', 'breakpoint']
+        })).on('error', notify.onError())
+        .pipe(gulp.dest(outputDir + "css"))
+        .pipe(browsersync.stream());
 });
 
 gulp.task('serve', ["compass"], function() {
 
-	    browsersync.init({
-	        server: "builds/develpment/"
+    browsersync.init({
+        server: "builds/develpment/"
     });
 
-	gulp.watch(["components/style/**/*.scss"], ['compass']);
-	gulp.watch("./builds/develpment/*.html").on('change', browsersync.reload);
-	gulp.watch("./builds/develpment/js/*.js").on('change', browsersync.reload);
+    gulp.watch(["components/style/**/*.scss"], ['compass']);
+    gulp.watch("./builds/develpment/*.html").on('change', browsersync.reload);
+    gulp.watch("./builds/develpment/js/*.js").on('change', browsersync.reload);
 })
 
 gulp.task('default', ['serve']);
